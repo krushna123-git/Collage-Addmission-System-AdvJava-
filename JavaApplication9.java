2fb7a837b8688c62e1f6487f457fee96340e0a8a
Krushna Kumbhare <73023164+krushna123-git@users.noreply.github.com>
2021-11-09 19:53:51 +0530

    Code Files Added

---
+++ JavaApplication9.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication9;
+
+/**
+ *
+ * @author Krushna
+ */
+public class JavaApplication9 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Mainform();
+      
+    }
+}
